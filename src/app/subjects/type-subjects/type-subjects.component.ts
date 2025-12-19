84eea5c52f249ba2aad9b60ee29be29bcf928110
Elankathir <esekar@diligentsquad.com>
2025-12-19 12:32:48 +0530

    updates in type-subjects

--- src/app/subjects/type-subjects/type-subjects.component.ts
+++ src/app/subjects/type-subjects/type-subjects.component.ts
@@ -4,12 +4,10 @@ import { AsyncSubject, BehaviorSubject, ReplaySubject } from 'rxjs';
 @Component({
   selector: 'app-type-subjects',
   templateUrl: './type-subjects.component.html',
-  styleUrls: ['./type-subjects.component.css']
+  styleUrls: ['./type-subjects.component.css'],
 })
 export class TypeSubjectsComponent {
-
   doBehaviourSub() {
-
     /*
     It holds the initial value, If no new value is emitted means, it will pass initial value
     to new subscriber.
@@ -18,17 +16,18 @@ export class TypeSubjectsComponent {
 
     const behaviourSubject = new BehaviorSubject<number>(100);
 
-    behaviourSubject.subscribe((res) => console.log("subscriber1 "+res));
+    behaviourSubject.subscribe((res) => console.log('subscriber1 ' + res));
 
     behaviourSubject.next(1000);
+    behaviourSubject.next(2000);
+    behaviourSubject.next(3000);
 
-    behaviourSubject.subscribe((res) => console.log("subscriber2 "+res));
+    behaviourSubject.subscribe((res) => console.log('subscriber2 ' + res));
 
-
+    behaviourSubject.next(4000);
   }
 
   doReplaySub() {
-
     /*
     replay subject replays old values to new subscribers
     it will collect all the emitted values in buffer, we can specify the buffer size
@@ -37,22 +36,23 @@ export class TypeSubjectsComponent {
 
     const replaySubject = new ReplaySubject(2);
 
-    replaySubject.next("a");
-    replaySubject.next("b");
-    replaySubject.next("c");
-    replaySubject.next("d");
+    replaySubject.next('a');
+    replaySubject.next('b');
+    replaySubject.next('c');
+    replaySubject.next('d');
 
-    replaySubject.subscribe((res) => console.log("subscriber1 "+res));
+    replaySubject.subscribe((res) => console.log('subscriber1 ' + res));
 
-    replaySubject.next("e");
+    replaySubject.next('e');
 
-    replaySubject.subscribe((res) => console.log("subscriber2 "+res));
+    replaySubject.subscribe((res) => console.log('subscriber2 ' + res));
 
-    replaySubject.next("f");
+    replaySubject.next('f');
+
+    replaySubject.subscribe((res) => console.log('subscriber3 ' + res));
   }
 
   doAsyncSub() {
-
     /*
     async subject will only pass last emitted value to all its subscribers
     once the complete method triggers.
@@ -62,13 +62,11 @@ export class TypeSubjectsComponent {
     asyncSubject.next(100);
     asyncSubject.next(200);
 
-    asyncSubject.subscribe((res) => console.log("subscriber1 "+res));
+    asyncSubject.subscribe((res) => console.log('subscriber1 ' + res));
 
     asyncSubject.next(300);
     asyncSubject.complete();
 
-    asyncSubject.subscribe((res) => console.log("subscriber2 "+res));
-
+    asyncSubject.subscribe((res) => console.log('subscriber2 ' + res));
   }
-
 }
